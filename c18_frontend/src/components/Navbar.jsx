367122b0e9aa78f42e824a0005004b85efab438e
Manuel Perez <66376102+manu4970@users.noreply.github.com>
2024-04-03 14:10:35 +0000

    Se actualiza Navbar y agrega react router dom

--- c18_frontend/src/components/Navbar.jsx
+++ c18_frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import logoDesafio from './icons/desafio-logo.png'
-import logoIncubadora from './icons/incubadora-logo.png'
+import logoDesafio from '../assets/icons/desafio-logo.png'
+import logoIncubadora from '../assets/icons/incubadora-logo.png'
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
 
@@ -7,10 +8,10 @@ export default function Navbar() {
 
     return(
         <nav className="flex justify-between items-center min-h-18 bg-[#9747FF]">
-            <div className='pl-4 py-2'>
+            <Link to='/' className='pl-4 py-2'>
                 <img src={logoIncubadora}></img>
                 <img className='ms-auto' src={logoDesafio}></img>
-            </div>
+            </Link>
         </nav>
     )
 }
