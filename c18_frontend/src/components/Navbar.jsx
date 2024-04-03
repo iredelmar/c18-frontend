dcdf0fcc4ef421f310b4dcb989bbc07be79aa28d
Manuel Perez <66376102+manu4970@users.noreply.github.com>
2024-04-03 15:46:36 +0000

    Se actualiza componente Navbar y se hace responsive

--- c18_frontend/src/components/Navbar.jsx
+++ c18_frontend/src/components/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
 
 
     return(
-        <nav className="flex justify-between items-center min-h-18 bg-[#9747FF]">
-            <Link to='/' className='pl-4 py-2'>
+        <nav className="flex justify-center rounded-t-2xl  md:justify-start md:rounded-none min-h-18 bg-[#9747FF] ">
+            <Link to='/' className='py-2 md:pl-4'>
                 <img src={logoIncubadora}></img>
                 <img className='ms-auto' src={logoDesafio}></img>
             </Link>
