48d1abf5cfc06cf26ed7dff6a486a28794827d3b
manu4970 <manu@PC>
2024-04-03 23:21:56 -0300

    Se crea navbar de vista metricas

--- c18_frontend/src/App.jsx
+++ c18_frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
+import NavMetricas from './components/NavMetricas/NavMetricas';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <NavMetricas />
       <h1 className="text-4xl font-bold underline bg-black text-white text-wrap top-3 ">Hello world!</h1>
     </>
   )
--- c18_frontend/src/components/Navbar.jsx
+++ c18_frontend/src/components/Navbar.jsx
@@ -1,10 +1,8 @@
+import { Link } from 'react-router-dom'
 import logoDesafio from '../assets/icons/desafio-logo.png'
 import logoIncubadora from '../assets/icons/incubadora-logo.png'
-import {Link} from 'react-router-dom'
 
 export default function Navbar() {
-
-
 
     return(
         <nav className="flex justify-center rounded-t-2xl  md:justify-start md:rounded-none min-h-18 bg-[#9747FF] ">
