77c871b97a293c170d9d1ae43d5588ca9b0215ca
desotvh <berendicto@gmail.com>
2024-04-27 17:31:36 -0400

    Cambios estilos

--- c18_frontend/src/components/login/LoginFooter.jsx
+++ c18_frontend/src/components/login/LoginFooter.jsx
@@ -1,6 +1,6 @@
 function LoginFooter() {
   return (
-    <div className="bg-[#9747FF] h-20 flex items-center justify-center fixed bottom-0 w-full">
+    <div className="bg-[#9747FF] h-20 flex items-center justify-center fixed bottom-0 w-full md:rounded-none rounded-b-2xl ">
       <img src="src\assets\icons\incubadora-logo.png" />
     </div>
   );
