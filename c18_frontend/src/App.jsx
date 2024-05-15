27e1b1dc37d27f3fb0881ee6603a1806afb37d31
Luis <30819865+hlcxpl@users.noreply.github.com>
2024-05-14 23:49:26 -0400

    Update App.jsx

--- c18_frontend/src/App.jsx
+++ c18_frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
 import Login from './components/login/Login';
 
 function App() {
@@ -10,15 +6,11 @@ function App() {
     <>
       <BrowserRouter>
         <div>
-          <div className="flex text-sm">
-            <Sidebar />
-
-            <div className="flex-grow"></div>
+            <Login />
           </div>
         </div>
       </BrowserRouter>
-      <Navbar />
-      <Login />
+     
     </>
   );
 }
