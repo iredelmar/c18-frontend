911b28746c367df839741ad4c81e38aefa5db9ca
Luis <30819865+hlcxpl@users.noreply.github.com>
2024-05-15 00:21:14 -0400

    Update App.jsx

--- c18_frontend/src/App.jsx
+++ c18_frontend/src/App.jsx
@@ -1,14 +1,12 @@
-
 import { BrowserRouter } from 'react-router-dom';
 import Login from './components/login/Login';
-
+import NavBar from './components/navbar/Navbar'
 function App() {
   return (
     <>
       <BrowserRouter>
         <div>
             <Login />
-          </div>
         </div>
       </BrowserRouter>
      
